d07b25eaf455cbbf3cfb7d08d929a8978c2a7794
FullDad <84098078+FullDad@users.noreply.github.com>
2022-08-31 13:09:57 -0400

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -o /tmp/CbDefenseInstall.pkg https://morriseytemp.blob.core.windows.net/deploy/CBCloud%20Install.pkg
-curl -o /tmp/cbdefense_install_unattended.sh https://morriseytemp.blob.core.windows.net/deploy/cbcloud_install_unattended.sh
+curl -o /tmp/CbDefenseInstall.pkg https://github.com/FullDad/carbonblack_1/blob/main/CBCloud_Install.pkg
+curl -o /tmp/cbdefense_install_unattended.sh https://raw.githubusercontent.com/FullDad/carbonblack_1/main/cbcloud_install_unattended.sh
 
 sudo sh ./tmp/cbdefense_install_unattended.sh -i '/tmp/CbDefenseInstall.pkg' -c 'SRGUXGAOW@J41GEW@IY' --skip-kext-approval-check
 
